824e72cb2b1b76a8f0ff1b624437c7b972319109
Harrebow <91289089+Harrebow@users.noreply.github.com>
2022-06-22 15:42:52 +0100

    Update Leap Year Calculator.py

--- Leap Year Calculator.py
+++ Leap Year Calculator.py
@@ -1,17 +1,12 @@
-exit_word = "test"
-count = 0
-user_word = 0
+year = int(input("Enter a year: "))
 
-while user_word != exit_word:
-    if user_word == exit_word:
-        print("You've successfully left the loop.")
-        break
-    else:
-        count += 1
-        if count <= 3:
-            user_word = (input("Please enter the Secret exit word: "))
-        else:
-            print("You've entered the Secret word incorrectly 3 times. You have now been locked out of the system.")
-            break
-
-print("Goodbye")
+if year < 1582:
+    print("Not within the Gregorian calendar period, which began in 1582.")
+if year % 4 != 0:
+    print("common year")
+elif year % 100 != 0:
+    print("leap year")
+elif year % 400 != 0:
+    print("common year")
+else:
+    print("leap year")
