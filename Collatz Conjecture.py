a577dec9174860f294206c0d1a2758d4d13bf5ff
Harrebow <91289089+Harrebow@users.noreply.github.com>
2022-06-22 15:42:04 +0100

    Update Collatz Conjecture.py

--- Collatz Conjecture.py
+++ Collatz Conjecture.py
@@ -1,65 +1,13 @@
-# for i in range(0, 11):
-#     if i % 2 != 0:
-#         print (i)
+c0 = int(input("Enter a number here: "))
+steps = 0
 
-
-# x = 1
-# while x < 11:
-#     if x % 2 != 0:
-#         print(x)
-#     x += 1
-
-
-# for ch in "john.smith@pythoninstitute.org":
-#     if ch == "@":
-#         break
-#     print(ch, end="")
-
-
-# for digit in "0165031806510":
-#     if digit == "0":
-#         print("x", end="")
-#         continue
-#     print(digit, end="")
-
-import datetime
-a = []
-
-def test():
-    count = 0
-    for i in range(0, 100001):
-        if i % 2 != 0:
-            count+=1
-            print(i)
-    
-    # count = 0
-    # i = 1
-    # while i < 100001:
-    #     if i % 2 != 0:
-    #         count+=1
-    #         print(i)
-    #     i += 1
-
-    # i=1
-    # while i != 100001:
-    #     print(i)
-    #     i+=2
-
-
-y = datetime.datetime.now()
-test()
-x = datetime.datetime.now()
-z=x-y
-a.append(z)
-y = datetime.datetime.now()
-test()
-x = datetime.datetime.now()
-z=x-y
-a.append(z)
-y = datetime.datetime.now()
-test()
-x = datetime.datetime.now()
-z=x-y
-a.append(z)
-
-print(a)
+while c0 != 1:
+    if c0 % 2 == 0:
+        c0 = c0 // 2
+        print(c0)
+        steps += 1
+    else:
+        c0 = 3 * c0 + 1
+        print(c0)
+        steps += 1
+print("Steps =", steps)
