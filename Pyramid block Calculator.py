e4a58263ddc13df59ddc854da0df879b8b4d291d
Harrebow <91289089+Harrebow@users.noreply.github.com>
2022-06-22 15:43:27 +0100

    Update Pyramid block Calculator.py

--- Pyramid block Calculator.py
+++ Pyramid block Calculator.py
@@ -1,13 +1,17 @@
-c0 = int(input("Enter a number here: "))
-steps = 0
+blocks = int(input("Enter the number of blocks: "))
+blocks_left = blocks
 
-while c0 != 1:
-    if c0 % 2 == 0:
-        c0 = c0 // 2
-        print(c0)
-        steps += 1
-    else:
-        c0 = 3 * c0 + 1
-        print(c0)
-        steps += 1
-print("Steps =", steps)
+i = -1
+layer = 0
+
+while blocks_left > layer:
+    blocks_left = blocks_left + i
+    count = (i - (i*2))
+    i -= 1
+    layer += 1
+    blocks_used = blocks - blocks_left
+    print("\nLayer:", layer, "\nNumber of blocks in layer", layer, "is:", count, "\nTotal blocks used so far:", blocks_used, "\nBlocks left:", blocks_left)
+    blocks_used = blocks - blocks_left
+
+height = layer
+print("\n-- The height of the pyramid:", height, "--\n-- Remaining blocks:", blocks_left, "--")
